1ca4096f430a957b0bfbe08e694daac351e15d44
peedronha <blackcraft177@gmail.com>
2024-04-11 12:50:38 -0300

    Correções

--- buddybridge-backend/src/main/java/br/com/buddybridge/core/security/WebSecurity.java
+++ buddybridge-backend/src/main/java/br/com/buddybridge/core/security/WebSecurity.java
@@ -35,32 +35,6 @@ public class WebSecurity{
 
     return http.build();
     }
-    @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-            throws IOException, ServletException {
-        HttpServletResponse response = (HttpServletResponse) res;
-        HttpServletRequest request = (HttpServletRequest) req;
-
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Max-Age", "3600");
-
-        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-            response.setStatus(HttpServletResponse.SC_OK);
-        } else {
-            chain.doFilter(req, res);
-        }
-    }
-
-    @Override
-    public void init(FilterConfig filterConfig) {
-    }
-
-    @Override
-    public void destroy() {
-    }
 }
 //                .cors().and()
 //                .csrf().disable().authorizeHttpRequests()
